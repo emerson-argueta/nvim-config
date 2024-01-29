91b183076b624c610e751219a1c18a2637ee3606
emerson.argueta <emerson.argueta@springscs.org>
2024-01-29 05:41:00 -0800

    using fzf for finder

--- after/plugin/finder.lua
+++ after/plugin/finder.lua
@@ -3,8 +3,17 @@ local telescope = require('telescope')
 telescope.setup {
 	pickers = {
 		find_files = { hidden = true }
-	}
+	},
+	extensions = {
+	    fzf = {
+	      fuzzy = true,                    -- false will only do exact matching
+	      override_generic_sorter = true,  -- override the generic sorter
+	      override_file_sorter = true,     -- override the file sorter
+    	}
+  }
 }
+
+require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
