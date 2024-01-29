5f3376630db77ca0bfc8c5f350f9e0f5ee94c54d
emerson.argueta <emerson.argueta@springscs.org>
2024-01-29 15:40:45 -0800

    added erb tags

--- after/plugin/ruby-on-rails.lua
+++ after/plugin/ruby-on-rails.lua
@@ -35,3 +35,26 @@ require("ror").setup({
 -- Set a keybind
 -- This "list_commands()" will show a list of all the available commands to run
 vim.keymap.set("n", "<Leader>fr", ":lua require('ror.commands').list_commands()<CR>", { silent = true })
+
+-- Function to insert ERB tags
+local function insert_erb_tag(tag_type)
+    local tag = ""
+    if tag_type == "output" then
+        tag = "<%=  %>"
+    elseif tag_type == "execution" then
+        tag = "<%  %>"
+    elseif tag_type == "execution_trim" then
+        tag = "<%-  %>"
+    end
+    vim.api.nvim_put({tag}, 'c', true, true)
+    -- Move cursor inside the brackets
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Left><Left><Left>', true, false, true), 'n', true)
+end
+-- Make the function globally accessible
+_G.insert_erb_tag = insert_erb_tag
+
+-- Map the function to different keybindings
+vim.api.nvim_set_keymap('i', '<leader><', '<C-o>:lua insert_erb_tag("output")<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<leader>>', '<C-o>:lua insert_erb_tag("execution")<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<leader>,', '<C-o>:lua insert_erb_tag("execution_trim")<CR>', { noremap = true, silent = true })
+
