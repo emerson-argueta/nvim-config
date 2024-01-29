a55dd20165861e082f4bce1b604aa3c351537d41
emerson.argueta <emerson.argueta@springscs.org>
2024-01-29 07:28:24 -0800

    arrow up and down

--- lua/vimide/remap.lua
+++ lua/vimide/remap.lua
@@ -2,4 +2,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>sa', 'ggVG', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('c', '<Up>', '<C-p>', { noremap = true })
+vim.api.nvim_set_keymap('c', '<Down>', '<C-n>', { noremap = true })
 
