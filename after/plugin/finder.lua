d31eec9cd20e64b9a827af28f894f6f6007bed60
emerson.argueta <emerson.argueta@springscs.org>
2024-01-28 18:52:20 -0800

    updated lsp

--- after/plugin/finder.lua
+++ after/plugin/finder.lua
@@ -10,5 +10,3 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.api.nvim_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', {noremap = true, silent = true})
-
