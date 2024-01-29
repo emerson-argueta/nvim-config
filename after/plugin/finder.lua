2580d9233f98e5e5d72f36c6f4aed74f35ff3227
emerson.argueta <emerson.argueta@springscs.org>
2024-01-29 06:36:34 -0800

    added git shortcuts

--- after/plugin/finder.lua
+++ after/plugin/finder.lua
@@ -15,7 +15,18 @@ telescope.setup {
 
 telescope.load_extension('fzf')
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>fgs', builtin.git_status, { desc = '' })
+
