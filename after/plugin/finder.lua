2417b7eb1056f55a91fb1a25a9ee78e830ccba1d
emerson.argueta <emerson.argueta@springscs.org>
2024-01-29 07:05:09 -0800

    adding preview back in

--- after/plugin/finder.lua
+++ after/plugin/finder.lua
@@ -19,7 +19,6 @@ vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently o
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
