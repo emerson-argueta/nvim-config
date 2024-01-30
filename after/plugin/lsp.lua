4ea3128090fcd909c419e1c39f3525bb32851b0d
emerson.argueta <emerson.argueta@springscs.org>
2024-01-30 15:03:27 -0800

    added fuzzy_ruby_server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -98,4 +98,65 @@ for _, ls in ipairs(language_servers) do
 end
 require('ufo').setup()
 vim.api.nvim_set_keymap('n', '<leader>af', ':LspZeroFormat<CR>', { noremap = true, silent = true })
+local lspconfig = require('lspconfig')
 
+-- Ruby LSP setup
+lspconfig.ruby_ls.setup {
+    filetypes = { "ruby", "eruby" }
+}
+
+-- HTML LSP setup
+lspconfig.html.setup {
+    filetypes = { "html", "eruby" }
+}
+
+-- fuzzy ruby server setup
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+local lspconfig = require('lspconfig')
+local configs = require 'lspconfig.configs'
+
+if not configs.fuzzy_ls then
+  configs.fuzzy_ls = {
+    default_config = {
+      cmd = {'fuzzy'};
+      filetypes = {'ruby'};
+      root_dir = function(fname)
+        return lspconfig.util.find_git_ancestor(fname)
+      end;
+      settings = {};
+      init_options = {
+        allocationType = "ram",
+        indexGems = true,
+        reportDiagnostics = true
+      };
+    };
+  }
+end
+
+lspconfig.fuzzy_ls.setup {
+    on_attach = on_attach
+}
